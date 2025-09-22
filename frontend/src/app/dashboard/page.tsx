e8470edf77e7944ec0f4982857b37e3b273cf5f9
aadi <heyaadi2@gmail.com>
2025-09-22 19:41:33 +0530

    basic initialization

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+     return <h1>Welcome to Cloud-Native Platform Dashboard</h1>; 
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Cloud-native platform</h1>
+    <Link href="/dashboard">Go to dashboard</Link>
+    </>
+  );
+}
